35247f83fa6f8ea9cfa3043e8d0a9e8eedcc9115
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-04 15:48:03 +0530

    Integrated material tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Input, Button } from "@material-tailwind/react";
+
+const InputComponent:React.ForwardRefExoticComponent<any> = Input;
 
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">
+      <div className="text-3xl font-bold underline m-20">
         Higway Delight
       </div>
+      <InputComponent
+        label="Name"
+        className="mt-100"
+      />
     </>
   )
 }
