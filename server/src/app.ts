19a8237438c2b81ebddacf1f41c78d9ad2b0fd9e
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-05 01:07:11 +0530

    Integrated typescript and nodemon in server

---
+++ server/src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// TODO: Change this to the whitelisted origins (before deploying)
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.use(cookieParser());
+
+export default app;
---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+require("dotenv").config({
+    path: `${__dirname}/../.env`
+});
+
+import app from "./app";
+import connectDB from "./db/connectDB";
+
+const port = process.env.PORT || 4000;
+
+//database connection
+connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            if (err instanceof Error) {
+                console.log("ERROR (App error): " + err?.message);
+            } else {
+                console.log('ERROR (App error): An unknown error occurred');
+            }
+        });
+
+        app.listen(port, () => {
+            console.log(`Server listening on ${port}\n`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed: ", err);
+    });
