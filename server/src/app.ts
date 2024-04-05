f90e0c591c07098f0e61c7fbff1619ef31527641
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-05 21:56:33 +0530

    Updated Welcome page css and modify cors options

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 // TODO: Change this to the whitelisted origins (before deploying)
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
