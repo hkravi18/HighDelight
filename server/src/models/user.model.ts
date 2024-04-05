3d06ac6c31bc7231b1b21c61f1b0e216a60d0168
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-05 12:47:55 +0530

    Added User database model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      regex: /^\S+@\S+\.\S+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    resetPasswordToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
